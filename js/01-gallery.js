f68a25c7a9b8b9043dd93392e91207e6101fb799
Andrii Romanovskyi <romanovskyi2137@gmail.com>
2022-12-04 19:45:45 +0200

    html render function is implemented

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="small-image.jpg"
+    data-source="large-image.jpg"
+    alt="Image description"
+  />
+</a>
+</div> */
+const gallery = document.querySelector('.gallery') 
 
 
+function renderGallery(){
+    let htmlString = galleryItems.reduce((acc, {preview, original, description}) => 
+      (acc + `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+      </div>`), '');
+    return gallery.insertAdjacentHTML('beforeend', `${htmlString}`)
+    
+}
+
+renderGallery();
