6dcb25fc20d649ee6fc781639d15db923b6dce62
Andrii Romanovskyi <romanovskyi2137@gmail.com>
2022-12-05 19:48:32 +0200

    simpleLightbox is implemented

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery'); 
+
+
+gallery.addEventListener('click', onPictureClick);
+
+
+let lightbox;
+
+function onPictureClick(event){
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+      return;
+    } if (event.target.nodeName === 'IMG'){
+       return lightbox = new SimpleLightbox('.gallery a', 
+        {
+            captions: true,
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionDelay: 250,
+        });
+  
+    }
+  };
+
+
+
+function renderGallery(){
+    let htmlString = galleryItems.reduce((acc, {preview, original, description}) => 
+      (acc + `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`), '');
+    return gallery.insertAdjacentHTML('beforeend', `${htmlString}`)  
+};
+renderGallery();
